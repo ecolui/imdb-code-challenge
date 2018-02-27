62c74e9c22344ec1e68c37f8da09088d4c8be22b
Luis Somoza <Luis.Somoza.LS@gmail.com>
2018-02-27 12:00:27 -0500

    cleanup

--- src/om_tut/server_data.clj
+++ src/om_tut/server_data.clj
@@ -1,34 +1,25 @@
-(ns om-tut.server-data
-  )
+(ns om-tut.server-data)
 
 ;;IMPORTANT TODO: Data was imported from text files @ 
 ;;Later on, Create a full blown, Odata driven web api
 ;;so that the client can query the data directly via an odata url
-
-
 
 ;;List contains only actors born after 1930 that are still alive.
 ;;Records with no birthyear are omitted from the cache
 (defonce actor-data
   (atom
     ;;NOTE: If the atom is empty, it could be that your
-    ;;input is encoded as ANSI windows-1252.
-    ;;(read-string (slurp "resources/public/imdb_data/actors.txt"))
+    ;;input is encoded as UTF-8 BOM, instead of just UTF-8.
     (read-string (slurp "resources/public/imdb_data/actors.txt"))
-    )
-  )
+    ))
 
 (defonce movie-data 
   (atom 
     {
       :welcome-msg "Welcome to the top movies"
       :top-movies 
-        ;;NOTE: If the atom is empty, it could be that your
-        ;;input is encoded as ANSI windows-1252.
         (read-string (slurp "resources/public/imdb_data/topmovies.txt"))        
-    }
-    )
-  )
+    }))
 
 
 
